8b9b4376d7348538388e851cbabb9733700f6f74
Tymianek <tymon.oziemblewski@gmail.com>
2017-09-04 00:36:21 +0200

    Δ

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
-class Δ {
-    constructor(value = 0, updateFunction = null) {
-        this.value = value;
-        if (updateFunction === null)
-        if (typeof performance === "object")
-        if (typeof performance.now === "function"){
-            this.updateFunction = performance.now;
+module.exports =
+    class Δ {
+        constructor(current = null, updateFunction = null, differenceFunction = null) {
+            this._changed = false;
+            if (typeof updateFunction === "function") {
+                this.updateFunction = updateFunction;
+            } else if (typeof performance === "object" && typeof performance.now === "function") {
+                this.updateFunction = performance.now;
+            } else if (typeof performanceNow === "function") {
+                this.updateFunction = performanceNow;
+            } else {
+                this.updateFunction = Date.now;
+            }
+            if (current == null)this.update();
+            if (current == null)this._previous = this._current;
+            if(typeof differenceFunction === "function"){
+                this.differenceFunction = differenceFunction;
+            }
         }
-        else if (typeof performanceNow === "function") {
-            this.updateFunction = performanceNow;
+        get current() {
+            return this._current;
         }
-        else if (typeof Date.now === "function"){
-            this.updateFunction = Date.now;
+        get previous(){
+            return this._previous;
         }
-    }
-    get value() {
-        return this.value;
-    }
-    set value(x) {
-        this.previousValue = this.value;
-        this.value = x;
-        this.changed = true;
-    }
-    get difference() {
-        if (this.changed) {
-            this.difference = this.differenceFunction(this.value, this.previousValue);
-            this.changed = false;
+        set current(x) {
+            this._previous = this._current;
+            this._current = x;
+            this._changed = true;
         }
-        return this.difference;
-    }
-    differenceFunction(x1, x2, ε = Number.EPSILON) {
-        if (Math.abs(x1 - x2) < ε) return 0;
-        else return x1 - x2;
-    }
-    updateFunction() {
-
-    }
-    update() {
-        updateFunction();
-    }
-    valueOf() {
-        return this.difference;
-    }
-
-}
+        get difference() {
+            if (this._changed) {
+                this._difference = this.differenceFunction(this._current, this._previous);
+                this._changed = false;
+            }
+            return this._difference;
+        }
+        differenceFunction(x1, x2, ε = Number.EPSILON) {
+            if (Math.abs(x1 - x2) < ε) return 0;
+            else return x1 - x2;
+        }
+        update() {
+            this.current = this.updateFunction();
+            return this.current;
+        }
+        valueOf() {
+            return this.difference;
+        }
+    };
